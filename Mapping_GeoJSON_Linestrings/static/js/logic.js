899cbd21bb3355938e87c63bff64386b20349195
rdebru <rdebru6@gmail.com>
2022-02-25 21:33:48 -0500

    Adding Earthquakes_past7days files

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -98,7 +98,7 @@ let sanFranAirport =
 //    accessToken: API_KEY
 //});
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -112,36 +112,39 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Street: light,
   Dark: dark
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
     center: [30, 30],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
-
+//light.addTo(map);
+// Create a style for the lines.
+let myStyle = {
+    color: "yellow",
+    weight: 2
+}
 // Accessing the airport GeoJSON URL
 //let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
-let airportData = "https://raw.githubusercontent.com/rdebru/Mapping_Earthquakes/main/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/rdebru/Mapping_Earthquakes/main/torontoRoutes.json";
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(toroData).then(function(data) {
     console.log(data);
 
   // Creating a GeoJSON layer with the retrieved data.
+   //L.geoJSON(data).addTo(map);
   L.geoJSON(data, {
+  style:myStyle,
   onEachFeature: function(feature, layer) {
-
-
-
-   layer.bindPopup("<h2> Airport code:" + feature.properties.faa + "</h2><hr><h2>Airport name: " + feature.properties.name + "</h2>");
+   layer.bindPopup("<h2> Airport code:" + feature.properties.airline + "</h2><hr><h2>Airport name: " + feature.properties.dst + "</h2>");
 }
 }).addTo(map);
 });
